3f0adbe26a7cbe63a72264852178ad8bb93f4f69
Dhannnnshree <dpachpor082@gmail.com>
2025-07-19 19:42:06 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser');
+const { CheckForAuthenticationCookie } = require('./middlewares/auth');
+
+const Blog = require("./models/blog")
+
+const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+
+const app = express();
+const PORT = 8000;
+
+mongoose.connect('mongodb://localhost:27017/blogify')
+.then((e) => console.log("MongoDB Connected"));
+
+app.set('view engine','ejs');
+app.set('views',path.resolve("./views"));
+
+app.use(express.urlencoded({ extended:false}));
+app.use(cookieParser());
+app.use(CheckForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
+
+app.get('/', async(req,res) => {
+    const allBlogs = await Blog.find({});
+    res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+    });
+});
+
+app.use("/user",userRoute);
+app.use("/blog",blogRoute);
+app.listen(PORT, () =>console.log(`Server Started at PORT:${PORT}`));
