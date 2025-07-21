c7542bd731ee6294e058df28822ead4b0bea5b8f
Dhannnnshree <dpachpor082@gmail.com>
2025-07-21 10:46:33 +0530

    Replaced index.js with app.js, updated nav and package files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
@@ -10,9 +12,10 @@ const userRoute = require('./routes/user');
 const blogRoute = require('./routes/blog');
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-mongoose.connect('mongodb://localhost:27017/blogify')
+mongoose
+.connect(process.env.MONGO_URL)
 .then((e) => console.log("MongoDB Connected"));
 
 app.set('view engine','ejs');
